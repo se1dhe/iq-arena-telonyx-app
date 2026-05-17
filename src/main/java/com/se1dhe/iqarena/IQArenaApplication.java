3d6d1d0630b9d08c5591200ba6714e5a189f384c
se1dhe <91713064+se1dhe@users.noreply.github.com>
2026-05-17 10:26:48 +0300

    Add application entrypoint

---
+++ src/main/java/com/se1dhe/iqarena/IQArenaApplication.java
@@ -0,0 +1,12 @@
+package com.se1dhe.iqarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Точка входа backend-приложения IQ Arena.
+@SpringBootApplication
+public class IQArenaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IQArenaApplication.class, args);
+    }
+}
