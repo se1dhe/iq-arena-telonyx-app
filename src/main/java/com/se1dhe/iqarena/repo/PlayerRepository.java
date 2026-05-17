289ecc91706ef47a12142ac12b8bd8291ca08d71
se1dhe <91713064+se1dhe@users.noreply.github.com>
2026-05-17 15:14:47 +0300

    Add Telegram lookup methods to PlayerRepository

--- src/main/java/com/se1dhe/iqarena/repo/PlayerRepository.java
+++ src/main/java/com/se1dhe/iqarena/repo/PlayerRepository.java
@@ -2,11 +2,14 @@ package com.se1dhe.iqarena.repo;
 
 import com.se1dhe.iqarena.domain.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 import java.util.UUID;
 
 // Репозиторий игроков.
 public interface PlayerRepository extends JpaRepository<Player, UUID> {
     Optional<Player> findByHandle(String handle);
+    Optional<Player> findByTelegramUserId(Long telegramUserId);
+    Optional<Player> findByReferralCode(String referralCode);
     boolean existsByHandle(String handle);
 }
